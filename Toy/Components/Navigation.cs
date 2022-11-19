9b792d26221d0cb8e108bcd54d7fe658d808c961
232025 <232025@WINMAC15>
2022-11-19 16:16:48 +0300

    Добавьте файлы проекта.

---
+++ Toy/Components/Navigation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.RightsManagement;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toy.Components
+{
+    internal class Navigation
+    {
+        public static bool isAuth = false;
+        public static MainWindow main;
+        public static User AuthUser = null;
+        public static List<User> navs = new List<User>();
+        public static void NextPage(User nav)
+        {
+            navs.Add(nav);
+            Update(nav);
+        }
+
+        private static void Update(User nav)
+        {
+    
+        }
+    }
+}
---
+++ Toy/Pages/AuthPages.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Toy.Components;
+using Toy.Pages;
+
+namespace Toy.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthPages.xaml
+    /// </summary>
+    public partial class AuthPages : Page
+    {
+        public AuthPages()
+        {
+            InitializeComponent();
+        }
+
+        private void AuthBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string login = LoginTb.Text;
+            string password = PasswordTb.Text;
+
+            if (login.Length == 0 && password.Length == 0)
+            {
+                MessageBox.Show("Заполни");
+
+            }
+            else
+            {
+                Navigation.AuthUser = DBConnect.db.User.ToList().Find(x => x.Login == login && x.Password == password);
+                if (Navigation.AuthUser == null)
+                    MessageBox.Show("Такого пол-я не существует");
+                else
+                {
+
+                }
+            }
+
+        }
+    }
+}
